792bb0d721caf37dd9c95440482670f7c85140bc
Jacob Bottelberghe <j.bottelberghe@gmail.com>
2015-10-01 22:33:54 -0600

    Add option for long-form detailed display

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include <boost/format.hpp>
 #include <boost/program_options.hpp>
 #include "StockQuote.hpp"
@@ -12,8 +13,9 @@ int main(int argc, char** argv) {
 		po::options_description options_desc;
 		po::options_description desc("Program Options");
 		desc.add_options()
-			("help,h", "produce help message")
+			("help,h", "show help message")
 			("detail,d", "show detailed view")
+			("long,l", "show long detailed view")
 			("show-name,n", "show names as well as symbols")
 			;
 		po::options_description hidden("Hidden options");
@@ -31,7 +33,7 @@ int main(int argc, char** argv) {
 		po::notify(vm); 
 
 		if (vm.count("help")) {
-			std::cout << "Usage: " << argv[0] << ": [options] symbol...]" << std::endl;
+			std::cout << "Usage: " << argv[0] << ": [options] symbol..." << std::endl;
 			std::cout << desc << std::endl;
 			return 1;
 		}
@@ -53,6 +55,19 @@ int main(int argc, char** argv) {
 						std::cout <<  boost::format("%-7s %7.2f %+7.2f %+7.2f%% %7.2f %7.2f %7.2f %7.2f %9.0f") 
 							% sq.symbol % sq.price % sq.change % sq.chg_percent % sq.day_low % sq.day_high % sq.year_low % sq.year_high % sq.volume  << std::endl;
 					}
+					else if (vm.count("long")) {
+						std::cout << std::fixed << std::setprecision(2)
+						<< "Name: " << sq.name << std::endl 
+						<< "Symbol: " << sq.symbol << std::endl 
+						<< "Price: " << sq.price << std::endl 
+						<< "Change: " << sq.change << std::endl 
+						<< "Change (%): " << sq.chg_percent << std::endl 
+						<< "Day Low: " << sq.day_low << std::endl 
+						<< "Day High: " << sq.day_high << std::endl 
+						<< "Year Low: " << sq.year_low << std::endl 
+						<< "Year High: " << sq.year_high << std::endl 
+						<< "Volume: " << sq.volume << std::endl;
+					}
 					else {
 						if (vm.count("show-name")) std::cout << sq.name << std::endl;
 						std::cout <<  boost::format("%-5s %7.2f  %+7.2f (%+5.2f%%)") % sq.symbol % sq.price % sq.change % sq.chg_percent << std::endl;
@@ -65,7 +80,7 @@ int main(int argc, char** argv) {
 			}
 		}
 		else {
-			std::cout << "You must specify one or more stock symbols." << std::endl;
+			std::cout << "You must specify one or more stock symbols. (-h for help)" << std::endl;
 			return 1;
 		}
 
